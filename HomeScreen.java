f698adeedb398a44b451be0cae06ae927a7276e3
abellc16 <42623115+abellc16@users.noreply.github.com>
2018-10-23 11:50:29 -0400

    Update HomeScreen.java

--- HomeScreen.java
+++ HomeScreen.java
@@ -1,13 +1,26 @@
 package com.example.android.frogger20;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class HomeScreen extends AppCompatActivity {
-
-    @Override
+public class HomeScreen extends AppCompatActivity
+{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
+
+        /**
+         *  Takes user from HomeScreen to SettingsScreen.
+         */
+        Button settingsBtn = (Button) findViewById(R.id.button_settings);
+        settingsBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(HomeScreen.this, SettingsScreen.class);
+                startActivity(intent);
+            }
+        });
     }
 }

d037a9e7987388eb3546e583d1c2fc0e7b83bc44
abellc16 <42623115+abellc16@users.noreply.github.com>
2018-10-23 19:21:34 -0400

    Update SettingsScreen.java

--- SettingsScreen.java
+++ SettingsScreen.java
@@ -2,12 +2,48 @@ package com.example.android.frogger20;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.*;
 
 public class SettingsScreen extends AppCompatActivity {
+    int diff = 1;
+    TextView EMH;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings_screen);
+
+        SeekBar seekBar_Difficulty = findViewById(R.id.seekBar_Difficulty);
+        seekBar_Difficulty.setOnSeekBarChangeListener(seekBarChangeListener);
+
+        int  progress = seekBar_Difficulty.getProgress();
+        EMH = findViewById(R.id.textView_EMH);
     }
+
+    SeekBar.OnSeekBarChangeListener seekBarChangeListener = new SeekBar.OnSeekBarChangeListener() {
+        @Override
+        public void onProgressChanged(SeekBar seekBar_Difficulty, int progress, boolean fromUser) {
+            if(progress < 34) {
+                EMH.setText("Easy");
+            }
+            else if(progress > 33 && progress < 67) {
+                EMH.setText("Medium");
+                diff = 2;
+            }
+            else if(progress > 66) {
+                EMH.setText("Hard");
+                diff = 3;
+            }
+        }
+
+        @Override
+        public void onStartTrackingTouch(SeekBar seekBar_Difficulty) {
+
+        }
+
+        @Override
+        public void onStopTrackingTouch(SeekBar seekBar_Difficulty) {
+
+        }
+    };
 }

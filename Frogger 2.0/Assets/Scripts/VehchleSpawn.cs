36d1782cc0a5be6867060c2ae66203b8067fb85f
Tim Gwaltney <gwaltneyt15@students.ecu.edu>
2018-12-04 15:26:10 -0500

    Car and Truck sprites and spawners
    Added car sprite and truck sprite.
    Creates spawners for both.

--- Frogger 2.0/Assets/Scripts/VehchleSpawn.cs
+++ Frogger 2.0/Assets/Scripts/VehchleSpawn.cs
@@ -1,27 +1,37 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class VehchleSpawn : MonoBehaviour {
 
-    public Rigidbody2D spawner;
+    //The game object that gets spawned.
     public GameObject vehicle;
+    //The rotation and change in possition of a spawn point.
+    public Transform[] spawnPoints;
 
-    public float spawnDelay = 10f;
-    float spawnTime = 0f;
-
+    //spawnTime is a counter for the spawning of a vehicle.
+    float spawnTime = 3f;
+    //spawnIncrement is the increment that spawnTime gets reduced.
+    public float spawnIncrement = 0.07f;
+    
+    //Update runs every second.
     private void Update()
     {
-        if(spawnTime <= Time.time)
+        if(spawnTime <= 0)
         {
             Spawn();
-            spawnTime = Time.time + spawnDelay;
+            spawnTime = 3f;
+        }
+        else
+        {
+            spawnTime = spawnTime - spawnIncrement;
         }
     }
 
+    //Spawns a car at a random index stored in point.
     void Spawn()
     {
-        Instantiate(vehicle);
+        int point = Random.Range(0, spawnPoints.Length);
+        Transform spawnPoint = spawnPoints[point];
+        Instantiate(vehicle, spawnPoint.position, spawnPoint.rotation);
     }
 
 }

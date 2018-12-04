3888080fc55fb166387a5f8682b13906f80b906e
Tim Gwaltney <gwaltneyt15@students.ecu.edu>
2018-12-04 11:26:27 -0500

    spawn points for cars
    working on spawn points for cars

---
+++ Frogger 2.0/Assets/Scripts/VehchleSpawn.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VehchleSpawn : MonoBehaviour {
+
+    public Rigidbody2D spawner;
+    public GameObject vehicle;
+
+    public float spawnDelay = 10f;
+    float spawnTime = 0f;
+
+    private void Update()
+    {
+        if(spawnTime <= Time.time)
+        {
+            Spawn();
+            spawnTime = Time.time + spawnDelay;
+        }
+    }
+
+    void Spawn()
+    {
+        Instantiate(vehicle);
+    }
+
+}

95fc5d529c9a6498e61c53b29aa655b5eaee6799
Tayjuana Mouring <mouringt15@students.ecu.edu>
2018-12-05 00:44:31 -0500

    score
    Scoring for game

---
+++ Frogger/Assets/Score.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour {
+    //public Transform player;
+    //public Text scoreText;
+    public float gameScore = 0;
+    public float previousscore = 0;
+    public Text sscore;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.touchCount > 0)
+        {
+            gameScore = previousscore + 10;
+            previousscore = gameScore;
+            Debug.Log(gameScore);
+            Debug.Log(previousscore);
+            sscore.text = gameScore.ToString();
+        }
+        else 
+        { 
+            gameScore = previousscore;
+        }  
+	}
+}

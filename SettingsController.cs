2d9e3b19db3987d0b0f3b777f680e469cfcfc2b4
abellc16 <42623115+abellc16@users.noreply.github.com>
2018-12-04 22:50:30 -0500

    Add files via upload

---
+++ SettingsController.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayController : MonoBehaviour {
+
+    public void NextScene()
+    {
+        SceneManager.LoadScene("SettingScreen");
+    }
+}
